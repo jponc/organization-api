b75edcd101661d217bfc1d9619438c606c8b74f7
Julian Ponce <julian@ponce.ph>
2015-07-15 19:00:59 +0800

    Added locations endpoint to get array of location names

---
+++ app/controllers/api/locations_controller.rb
@@ -0,0 +1,17 @@
+class Api::LocationsController < ApplicationController
+  def show
+    group_org = get_group_org
+    if group_org.present?
+      render json: group_org.location_names
+    else
+      render json: {msg: 'Group organization not found.'}
+    end
+
+  end
+
+  private
+
+  def get_group_org
+    GroupOrganization.where(id: params[:group_organization_id]).entries.first
+  end
+end
