6c4ba8524aeb2b4c64949c8f5b1fa1773b5ffdaf
Julian Ponce <julian@ponce.ph>
2015-07-15 18:56:23 +0800

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# GENERATE GROUP ORGANIZATION
+group_organization = GroupOrganization.create(name: 'Aureso', org_code: 'AUR')
+
+# GENERATE ORGANIZATIONS
+show_room = group_organization.organizations.create(
+  name: 'Aureso Show Room', public_name: 'Aureso Show Room',
+  org_type: Organization::ORG_TYPES[:SHOW_ROOM]
+)
+
+service = group_organization.organizations.create(
+  name: 'Aureso Service', public_name: 'Aureso Service',
+  org_type: Organization::ORG_TYPES[:SERVICE]
+)
+
+# GENERATE LOCATIONS
+show_room.locations.create([
+  {name: 'Aureso SF', address: 'San Francisco, California'},
+  {name: 'Aureso NY', address: 'New York, New York'},
+  {name: 'Aureso TX', address: 'San Austin, Texas'},
+])
+
+service.locations.create(name: 'Aureso SG', address: 'Singapore, Singapore')
